e2b3491e441846a16c1d472d421161a5db77718b
EstLopCor <estherlopezcordoncillo@gmail.com>
2021-05-07 10:32:57 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "../styleshets/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hola mundo
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styleshets/index.css";
+import App from "./components/App.js";
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
